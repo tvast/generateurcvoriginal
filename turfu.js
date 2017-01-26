ebd3fd825da0ee25e4f1280c37c368bce62fb536
institutfrancaistest <theophile.vast@institutfrancais.es>
2017-01-26 17:44:52 +0100

    change header and style the cv

--- turfu.js
+++ turfu.js
@@ -4,6 +4,7 @@ function toggleCv() {
   var foo = document.getElementById("questioncv").className ="toggleCv";
   var bar = document.getElementById("reponsecv").className +="toggledCv";  
 
+  //ciblage du formulaire
   var x = document.getElementById("frm1");
   var text = "";
           //tableau des réponses
@@ -20,8 +21,10 @@ function toggleCv() {
         adresse = array[0]+ " "+array[1]+ " "+array[2]+ " "+array[3]+ " "+array[4]+ " "+
         array[5]+ " "+array[6]+ " "+array[7]+ " "+array[8]+ " "+array[9]+ " "+array[10]+ " "+
         array[11]+ " "+array[12]+ " "+array[13];*/
+        //combien d'entité
         console.log(array.length)
 
+        //construction du cv
         text = ("");
         text = (text + "<strong><center>UN CV TRÈS SPÉCIAL !</strong></center><br>");
         text = (text + " " +array[0]+ " ");
@@ -37,7 +40,7 @@ function toggleCv() {
                 text = (text +"<br>Mes expériences de " +form.input8.value+ " et de  " +form.input7.value+ " ainsi que mes connaissances en "); 
                 text = (text +" " +form.input9.value+ " me permettent donc de prétendre à un poste de"); 
                 text = (text +" "+form.input16.value+". ");*/
-                text = (text +"<br>Pour ce poste mes prétentions sont de recevoir un salaire net mensuel de " +  " par mois! ");
+                text = (text +"<br>Pour ce poste mes prétentions sont de recevoir un salaire net mensuel de " +array[11]+  " par mois! ");
 
 
 
@@ -45,7 +48,7 @@ function toggleCv() {
 
                 console.log(adresse)
                 var bar = document.getElementById("tagline") 
-                bar.className +="toggleCv";
+                bar.className +="toggledCv";
               }
 
               
