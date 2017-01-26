a9b2745ae986d5ef792b1104b25cdd9f358de4ac
institutfrancaistest <theophile.vast@institutfrancais.es>
2017-01-26 16:45:26 +0100

    initial refonte commit

--- turfu.js
+++ turfu.js
@@ -18,10 +18,12 @@ function myFunction() {
             }
         // ecriture de la lettre avec differentes variables
         var adresse= "";
-        adresse = array[0]+ " "+array[1]+ " "+array[2]+ " "+array[3]+ " "+array[4]+ " "+array[5]+ " "+array[6]+ " "+array[7]+ " "+array[8]+ " "+array[9]+ " "+array[10]+ " "+array[11]+ " "+array[12]+ " "+array[13]+ " "+array[14]+ " "+array[15]+ " ";
+        adresse = array[0]+ " "+array[1]+ " "+array[2]+ " "+array[3]+ " "+array[4]+ " "+
+        array[5]+ " "+array[6]+ " "+array[7]+ " "+array[8]+ " "+array[9]+ " "+array[10]+ " "+
+        array[11]+ " "+array[12]+ " "+array[13];
         document.getElementById("demo1").innerHTML = adresse;
 
         console.log(adresse)
         var bar = document.getElementById("tagline") 
         bar.className +="toggleCv";
-      }
+      }  
