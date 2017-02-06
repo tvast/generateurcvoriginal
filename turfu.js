4c6abaf9ac6610d71633caa69b14b98b5bf13d38
theo vast <theovast@MacBook-Pro-de-theo.local>
2017-02-06 13:22:18 +0100

    change last detqil before clen up code

--- turfu.js
+++ turfu.js
@@ -11,7 +11,7 @@ function toggleCv() {
   var text = "";
   //tableau des réponses
   var array = [ ] 
-  var i;
+  var i;  
   //collecte des réponses
   for (i = 0; i < x.length ;i++) {
     // text += x.elements[i].value 
@@ -30,17 +30,23 @@ function toggleCv() {
     text = (text + " " +array[0]+ " ");
     text = (text + " " +array[1]+ "<br>12 rue " +array[2]+ " ");
     text = (text + "<br> " +array[3]+ " ");
-    text = (text + "<br> " +array[4]+ "ans<br><strong><center> " + array[5]+ "<br></strong></center> ");
-    text = (text + "<strong><br>FORMATION PROFESSIONNELLE</strong><br>" +array[6]+ "<br><strong>EXPERIENCE</strong><br> ");
-    text = (text + "  " +array[8]+ "(de 1995 à 1999) <br>" +array[7]+ "(de 1999 à 2003)<strong><br>LANGUE</strong><br>" +array[9]+ " ");
-    text = (text +" <br><strong>LOISIRS</strong><br> " +array[10]+ " <br>J'aime aussi manger des " +array[11]+ " et du " +array[12]+ " au soleil ");
-    text = (text +"");
-    text = (text +" <br><strong>MES ATOUTS</strong><br>" + " J'ai animé l'émission de télévision   " +array[13]+ " pendant trois ans.<br> ");
-    text = (text +"<br>Pour ce poste mes prétentions sont de recevoir un salaire net mensuel de " +array[11]+  " par mois! ");
+    text = (text + "<br> " +array[4]+ " ans<br><center><strong>" + array[5]+ "<br></center></strong> ");
+    text = (text + "<strong><br>FORMATION PROFESSIONNELLE</strong><br> " +array[6]+ "<br><strong>EXPERIENCE</strong><br> ");
+    text = (text + "  " +array[8]+ " (de 2000 à 2009) <br>" +array[7]+ " (de 2010 à 2016)<strong><br>LANGUE</strong><br> " +array[9]+ " courant");
+    text = (text + " <br><strong>LOISIRS</strong><br> " +array[10]+ " <br>J'aime aussi manger des " +array[11]+ " et du " +array[12]+ " au soleil ");
+    text = (text + " <br><strong>MES ATOUTS</strong><br>" + "J'ai animé l'émission de télévision " + array[13]+ " pendant trois ans.<br> ");
+    text = (text + "Jai écrit une biographie de " + array[13] + " parue le " + array[14] +" 2000.");
+    text = (text +" <br>Mes expériences de "+array[8]+" et de "+array[7]+" ainsi que mes connaissances en "+array[9]+" me permettent donc de prétendre à un poste de "+array[3]);
+    text = (text +"<br>Pour ce poste mes prétentions sont de recevoir un salaire net mensuel de " +array[11]+ " par mois! ");
    //ajoute le texte dans la balise p avec l'id demo1
     document.getElementById("demo1").innerHTML = text;
-    var bar = document.getElementById("tagline") 
-    bar.className +="toggledCv";
+  
+    // var bar = document.getElementById("tagline") 
+    // bar.className +="toggledCv";
+
               }
+
+            
+
 
               
