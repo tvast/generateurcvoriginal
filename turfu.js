4dec39811227dd6b2133e1fb2c322c9767f47553
institutfrancaistest <theophile.vast@institutfrancais.es>
2017-02-08 12:21:33 +0100

    clean up  text and add a modal

--- turfu.js
+++ turfu.js
@@ -29,16 +29,16 @@ function toggleCv() {
     text = (text + " " +array[0]+ " ");
     text = (text + " " +array[1]+ "<br>12 rue " +array[2]+ " ");
     text = (text + "<br> " +array[3]+ " ");
-    text = (text +"<br> " +array[4]+ " ans<br>");
+    text = (text +"<br> " +array[4]+ " ans");
     document.getElementById("head").innerHTML = text;
     text2 = ("<center><strong>" +array[5]+ "</center></strong><br> ");
     document.getElementById("title").innerHTML = text2;
-    text3 = ("<strong><br>FORMATION PROFESSIONNELLE</strong><br> " +array[6]+ "<br><strong>EXPERIENCE</strong><br> ");
-    text3 = (text3 + "  " +array[5]+ " (de 2000 à 2009) <br>" +array[7]+ " (de 2010 à 2016)<strong><br>LANGUE</strong><br> " +array[8]+ " courant");
-    text3 = (text3 + " <br><strong>LOISIRS</strong><br> " + "J'aime aussi manger des " +array[10]+ " et du " +array[11]+ " au soleil ");
-    text3 = (text3 + " <br><strong>MES ATOUTS</strong><br>" + "J'ai animé l'émission de télévision " + array[13]+ " pendant trois ans.<br> ");
+    text3 = ("<strong><br>FORMATION PROFESSIONNELLE</strong><br><br> " +array[6]+"<br>"+ "<br><strong>EXPERIENCE</strong><br><br> ");
+    text3 = (text3 + "  " +array[5]+ " (de 2000 à 2009) <br>" +array[7]+ " (de 2010 à 2016)<strong><br><br>LANGUE</strong><br><br> " +array[8]+ " courant<br>");
+    text3 = (text3 + " <br><strong>LOISIRS</strong><br><br> " +array[9]+ "J'aime aussi manger des " +array[10]+ " et du " +array[11]+ " au soleil<br> ");
+    text3 = (text3 + " <br><strong>MES ATOUTS</strong><br><br>" + "J'ai animé l'émission de télévision " + array[13]+ " pendant trois ans.<br><br> ");
     text3 = (text3 + "Jai écrit une biographie de " + array[14] + " parue le " + array[15]  );
-    text3 = (text3 +" <br>Mes expériences de "+array[8]+" et de "+array[7]+" ainsi que mes connaissances en "+array[9]+" me permettent donc de prétendre à un poste de "+array[5]);
+    text3 = (text3 +" <br>Mes expériences de "+array[8]+" et de "+array[7]+" ainsi que mes connaissances en "+array[9]+" me permettent donc de prétendre à un poste de "+array[5]+"<br>");
     text3 = (text3 +"<br>Pour ce poste mes prétentions sont de recevoir un salaire net mensuel de " +array[12]+ " par mois! ");
    //ajoute le texte dans la balise p avec l'id demo1
     document.getElementById("sub").innerHTML = text3;
